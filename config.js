7007060464b01f1a7271e2dfb6cb9d12cfdf46f7
Ethan Zhao <105615390+ethanhzhao@users.noreply.github.com>
2022-11-02 16:43:39 -0400

    added base bot files

---
+++ config.js
@@ -0,0 +1,8 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'Kc6tLIBCGA6mi0AszFCuipK1S',
+  consumer_secret:      'nioEmwhYJLyTLeGzsszVzUTNRY5uMFFcbjX71incPtKcFi4Oxd',
+  access_token:         '1586548846846304256-er3IeaQaJV8TsbF9Ebgiv5OfD0kOSZ',
+  access_token_secret:  '8jTiKP5fT2I7Lyy9MIQlGwuTG6AUoKmYzX5xHvXXlibfs'
+}
+// @LMC2700_F22 twit acc
